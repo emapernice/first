d668391ea049d870614a2ff2f0b5213eec2c907f
emapernice <perniceemanuel@gmail.com>
2023-08-21 22:36:53 -0300

    Agregando archivo app

---
+++ app.js
@@ -0,0 +1,10 @@
+console.log(saldosMecesGanancia([2, 10, -20]));
+function saldosMecesGanancia(unPeriodo) {
+    let saldos = [];
+    for (let i = 0; i < unPeriodo.length; i++) {
+        if (unPeriodo[i] > 0) {
+            saldos.push(unPeriodo[i]);
+        }     
+    }
+    return saldos;
+}
